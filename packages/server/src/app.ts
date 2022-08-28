113995a2311fc3e54fabebf117df07cee9867cb9
Kayoshi-dev <maxime.jme@gmail.com>
2022-08-28 18:24:41 +0200

    Add basic fastify setup

--- packages/server/src/app.ts
+++ packages/server/src/app.ts
@@ -1,1 +1,28 @@
-const a = 'b';
+import Fastify from 'fastify';
+
+import path from 'path';
+import autoLoad from '@fastify/autoload';
+
+const fastify = Fastify({ logger: true });
+
+fastify.register(require('@fastify/cors'), {
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+});
+
+fastify.register(autoLoad, {
+    dir: path.join(__dirname, 'plugins'),
+});
+
+fastify.register(autoLoad, {
+    dir: path.join(__dirname, 'routes'),
+    options: { prefix: '/api' }
+});
+
+fastify.listen({ port: 4000 }, (err) => {
+    if (err) {
+        console.error(err);
+        process.exit(1);
+    }
+    console.log('Server running on http://localhost:4000');
+});
